f45dde312e26120d77d068837ed4ec2797b3470f
Tommt354 <komonbass@gmail.com>
2026-06-03 17:43:24 +0300

    пп

--- server.js
+++ server.js
@@ -757,7 +757,7 @@ app.get("/api/orders/:id", authMiddleware, (req, res) => {
   if (!o) return res.status(404).json({ error: "Не знайдено" });
   if (req.user.role === "dropshipper" && o.dropshipper_id !== req.user.id) return res.status(403).json({ error: "Немає доступу" });
 
-  o.items = db.prepare(`SELECT oi.*,v.name as var_name,v.photo as var_photo,v.print_id,v.base_product_id,s.name as size_name,p.name as print_name,m.is_ready_product
+  o.items = db.prepare(`SELECT oi.*,v.name as var_name,v.photo as var_photo,bp.photo as bp_photo,p.photo as print_photo,v.print_id,v.base_product_id,s.name as size_name,p.name as print_name,m.is_ready_product
     FROM order_items oi JOIN variations v ON oi.variation_id=v.id JOIN sizes s ON oi.size_id=s.id LEFT JOIN prints p ON v.print_id=p.id JOIN base_products bp ON v.base_product_id=bp.id JOIN models m ON bp.model_id=m.id
     WHERE oi.order_id=?`).all(o.id);
 
@@ -868,7 +868,7 @@ app.get("/api/stock-returns", authMiddleware, (req, res) => {
 app.get("/api/orders/by-ttn/:ttn", authMiddleware, (req, res) => {
   const o = db.prepare("SELECT o.*,u.name as drop_name FROM orders o JOIN users u ON o.dropshipper_id=u.id WHERE o.ttn=? OR o.ttn_return=?").get(req.params.ttn, req.params.ttn);
   if (!o) return res.status(404).json({ error: "Замовлення з такою ТТН не знайдено" });
-  o.items = db.prepare(`SELECT oi.*,v.name as var_name,v.photo as var_photo,v.print_id,v.base_product_id,s.name as size_name,p.name as print_name,m.is_ready_product
+  o.items = db.prepare(`SELECT oi.*,v.name as var_name,v.photo as var_photo,bp.photo as bp_photo,p.photo as print_photo,v.print_id,v.base_product_id,s.name as size_name,p.name as print_name,m.is_ready_product
     FROM order_items oi JOIN variations v ON oi.variation_id=v.id JOIN sizes s ON oi.size_id=s.id LEFT JOIN prints p ON v.print_id=p.id JOIN base_products bp ON v.base_product_id=bp.id JOIN models m ON bp.model_id=m.id
     WHERE oi.order_id=?`).all(o.id);
   res.json({ order: o });
